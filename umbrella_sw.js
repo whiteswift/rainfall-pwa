98879c24c4078102ad096a792b4ddd734eb6eab9
Darryl White <whitedarryl101@gmail.com>
2018-01-08 15:02:52 +0000

    start_url query param, ignore search in sw

--- umbrella_sw.js
+++ umbrella_sw.js
@@ -27,7 +27,7 @@ self.addEventListener('activate',  event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
     })
   );
